dfb34046caf9e6e016906e671c9aa4f079201fc8
tanglejun <tanglejun@cvte.com>
2017-12-11 11:53:51 +0800

    [cclk]linux build

--- build/build-unix.sh
+++ build/build-unix.sh
@@ -13,7 +13,13 @@ then
     exit 1
 fi
 
-git rev-list HEAD | wc -l > version
+rm -rf ../../public/include/glog/*.*
+cp -f build_temp/glog/*.h ../../public/include/glog/
+cp -f ../src/glog/log_severity.h ../../public/include/glog/
+cp -f logger/logger.h ../../public/include/glog/
+
+rm -rf ../../public/so/libglog*
+cp -f build_temp/libglog.so* ../../public/so/
 
 exit 0
 
